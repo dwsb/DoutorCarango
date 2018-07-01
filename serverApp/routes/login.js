847d128aceeb15b6a712928ab350c2e74f439daa
debs <dfd2@cin.ufpe.br>
2018-07-01 19:22:07 -0300

    Resetar senha + tratamento de login user e oficina

--- serverApp/routes/login.js
+++ serverApp/routes/login.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var uuid = require('node-uuid');
 var router = express.Router();
 var config = require ('../config');
 var mysql = require('mysql');
+var bcrypt = require('bcrypt');
 
 connection = mysql.createConnection(config.database);
 connection.connect(function(err){
@@ -10,6 +12,8 @@ connection.connect(function(err){
 });
 
 global.db = connection;
+
+
 
 module.exports = function (app) {
 
@@ -20,15 +24,27 @@ module.exports = function (app) {
 
 	app.post('/login', function (req, res, next) {
 		
-		var login = req.body.username;
+		var login = req.body.email;
 		var pass  = req.body.password;
 		var sess = req.session; 
 		
-		var sql="SELECT id, `nome`,`cpf`,`email`,`login`, `senha` FROM `tb_usuarios` WHERE `login`='"+login+"' and senha = '"+pass+"'";                           
-      
+		var sql ="SELECT id, `nome`,`cpf`,`email`,`login`, `senha` FROM `tb_usuarios` WHERE `login`='"+email+"' and senha = '"+pass+"'";                           
+      	var sql2="SELECT id, `nome`,`cnpj`,`email`,`login`, `senha` FROM `tb_estabelecimentos` WHERE `login`='"+email+"' and senha = '"+pass+"'";                           
+      	
+
 		db.query(sql, function(err, results){      
          if(results.length){
             res.redirect('/users');
+         }
+         else{
+            message = 'Wrong Credentials.';
+            res.render('index.ejs',{message: message});
+         }                
+      });
+
+		db.query(sql2, function(err, results){      
+         if(results.length){
+            //res.redirect('/estabelecimentos'); //redirecionamento pra page de oficina
          }
          else{
             message = 'Wrong Credentials.';
@@ -72,5 +88,84 @@ module.exports = function (app) {
 		delete req.session.authenticated;
 		res.redirect('/');
 	});
+
+	app.post('/forgotPwd', function (req, res) {
+
+
+        var dateTime = Date();
+        console.log("Recuperacao solicitada!");
+        var pwdRequest = req.body.email;
+        var uuid1 = uuid.v1();
+        var map = {};
+        map.emailId = pwdRequest;
+        map.identifier = uuid1;
+        map.timestamp = dateTime;
+        var completeUpdatedList = [];
+        var completeUpdatedList = addLatestToList(map, mapList, listOfEmail);
+        console.log(JSON.stringify(mapList));
+        res.send("Resetar senha page:http://localhost:8080/restMyPassword?token=" + map.identifier);
+    });
+
+    app.post('/restMyPassword', function (req, res) {
+	    var password_change = req.body;
+	    var token = req.param('token');
+	    var password = password_change.password;
+	    var confirm_pwd = password_change.confirm_password;
+	    var email = password_change.email;
+	    if (password == confirm_pwd) {
+	        removeFromListAndMapOnSuccess(email)
+	        console.log(JSON.stringify(mapList));
+	        bcrypt.genSalt(saltRounds, function (err, salt) {
+	            bcrypt.hash(password, salt, function (err, hash) {
+	                var dbEntry = {};
+	                dbEntry.email = email;
+	                dbEntry.passwordHash = hash;
+	                database.collection("signIn").insertOne(dbEntry, function (err, res) {
+	                    if (err) throw err;
+	                    console.log("erro");
+	                    //db.close();
+	                });
+	            });
+	        });
+	        res.send("ok");
+	    }
+	    else {
+	        res.send("senhas diferentes");
+	    }
+	    console.log('senha recuperada id:' + "token" + token);
+	});
+
+	
+	var addLatestToList = function (map, mapListComplete, listOfEmail) {
+	    if (listOfEmail.indexOf(map.emailId) == -1) {
+	        listOfEmail.push(map.emailId);
+	        mapListComplete.push(map);
+	        return mapListComplete;
+	    }
+	    else {
+	        for (var i in mapList) {
+	            if (mapListComplete[i].emailId == map.emailId) {
+	                mapListComplete[i].identifier = map.identifier;
+	                mapListComplete[i].timestamp = map.timestamp;
+	            }
+	        }
+	    }
+	    return mapListComplete;
+	}
+
+	var removeFromListAndMapOnSuccess = function (email) {
+	    for (var i = 0; i < mapList.length; i++) {
+	        if (mapList[i].emailId == email) {
+	            mapList.splice(i, 1);
+	            console.log("retirando objeto");
+	        }
+	    }
+	    for (var i = 0; i < listOfEmail.length; i++) {
+	        if (listOfEmail[i] == email) {
+	            listOfEmail.splice(i, 1);
+	            console.log("retirando email");
+	        }
+	    }
+	}
 
 };
