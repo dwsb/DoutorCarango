61d9c30d37094f63df6fa5ecb3a78df17e19ea99
debs <dfd2@cin.ufpe.br>
2018-07-09 01:51:48 -0300

    Distancia calculo

---
+++ serverApp/distancia_teste.js
@@ -0,0 +1,11 @@
+var distance = require('google-distance');
+
+distance.get(
+  {
+    origin: 'San Francisco, CA',
+    destination: 'San Diego, CA'
+  },
+  function(err, data) {
+    if (err) return console.log(err);
+    console.log(data);
+});
