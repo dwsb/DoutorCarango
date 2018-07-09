8c39921898e89f28869502963105cf47792c1912
unknown <dwsb@cin.ufpe.br>
2018-07-08 21:55:58 -0300

    Adicionado povoamento real

---
+++ serverApp/PovoamentoReal.sql
@@ -0,0 +1,42 @@
+insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Oficina Medeiros', '1', 'medeirosoficina@gmail.com','0','123','Av.Joaquim Nabuco','759','Varadouro','Olinda', '53020-310', 'PE', 'Brasil', '','(81) 3439-1009',' ');
+
+insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Norte Centro Automotivo', '21.958.189/0001-08', 'norte.centroauto@gmail.com','1','123','Av. Norte Miguel Arraes de Alencar','7205','Macaxeira','Recife', '52091-000', 'PE', 
+'Brasil', 'Loja 03','(81) 99815-6746','(81) 3025-0646');
+
+insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Gimano Auto Service', '15.650.952/0001-28', 'gimanoauto.service@hotmail.com','1','123','Av. Norte Miguel Arraes de Alencar','7205','Macaxeira','Recife', '52091-000', 'PE',
+ 'Brasil', 'Loja 01','(81) 98865-9985','(81) 3266-6915');
+ 
+ insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Auto Gás', '05.522.328/0001-91', 'auto.gas@gmail.com','1','123','Av. Norte Miguel Arraes de Alencar','7205','Macaxeira','Recife', '52091-000', 'PE',
+ 'Brasil', 'Loja 02','(81) 98760-4036','(81) 3267-8198');
+ 
+ insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Auto Vip', '05.696.895/0001-91', 'auto.vip@gmail.com','1','123','Av. Presidente Kennedy','179','Santa Tereza','Olinda', '53010-120', 'PE',
+ 'Brasil', ' ','(81) 3439-9600',' ');
+ 
+insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Bultrins Autopeças e Serviços', '24.0934.377/0001-15', 'bultrins.autopecas@hotmail.com','1','123','Av. Bultrins','561','Bultrins','Olinda', '53320-170', 'PE',
+ 'Brasil', ' ','(81) 99519-1110','(81) 3439-4919');
+
+insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Vado Auto Peças e Serviçoes', '69.892.453/0001-76', 'vado.autopecas@gmail.com','1','123','Rua São Miguel','660','Bairro Novo','Olinda', '53030-155', 'PE',
+ 'Brasil', ' ','(81) 3439-8151',' ');
+ 
+ insert into TB_Estabelecimentos (nome, cnpj, email, credencia, senha, rua, numero, bairro, cidade, cep, 
+estado, pais, complemento, telefone1, telefone2) values 
+('Dikas Auto Pecas', '2', 'dicas.autop@gmail.com','0','123','Av. Zequinha Barreto','20','Piedade','Jaboatão dos Guararapes', '54400-090', 'PE',
+ 'Brasil', ' ','(81) 3342-3266',' ');
+ 
+ 
+ 
+ 
